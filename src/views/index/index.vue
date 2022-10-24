bec99d067197e3b8074d1775e0a7720154afb797
解晓东 <1320754724@qq.com>
2022-10-24 21:47:29 +0800

    第一模块静态页面

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="wrap-abs">
-        <comp-container class="module-first" text="标题">
-            <div class="content-first"></div>
+        <comp-container class="module-first" text="空气质量">
+            <div class="content-first">
+                <div class="content-left"></div>
+                <div class="content-right">
+                    <div
+                        class="pollute-nav"
+                        v-for="(item, index) in aPollute"
+                        :key="index"
+                    >
+                        <div class="pollute-name">{{ item.name }}</div>
+                        <div class="pollute-val">{{ item.val }}</div>
+                    </div>
+                </div>
+            </div>
         </comp-container>
     </div>
 </template>
@@ -11,7 +23,16 @@ import CompContainer from '@/components/common/container.vue';
 export default {
     components: { CompContainer },
     data() {
-        return {};
+        return {
+            aPollute: [
+                { name: 'PM2.5', val: '26.11' },
+                { name: 'PM10', val: '37.0' },
+                { name: 'SO2', val: '3.0' },
+                { name: 'NO2', val: '11.22' },
+                { name: 'O3', val: '69.0' },
+                { name: 'CO', val: '0.85' },
+            ],
+        };
     },
     mounted() {},
     beforeDestroy() {},
@@ -28,5 +49,41 @@ export default {
     height: 400px;
 }
 .content-first {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+}
+.content-left {
+    width: 300px;
+    height: 300px;
+    background: url(~@/assets/images/components/project/index/left-content.png)
+        no-repeat center;
+    background-size: 100% 100%;
+}
+.content-right {
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.pollute-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 220px;
+    height: 60px;
+    margin-top: 10px;
+    padding: 0 15px;
+    background-color: #1c425c;
+}
+.pollute-name {
+    font-size: 18px;
+    color: #fff;
+}
+.pollute-val {
+    font-size: 24px;
+    color: #67b243;
 }
 </style>
